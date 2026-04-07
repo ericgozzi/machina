782b2d8d4cba9956761b782fb2ff3e8aa5537d04
Gozzi  Eric <ericgozzi@cosmicgarage.ch>
2026-04-07 16:10:11 +0200

    picture blank and colors module

---
+++ src/machina/colors/__init__.py
@@ -0,0 +1,29 @@
+from .color import Color
+from .standard_colors import (
+    BLACK,
+    BLUE,
+    CYAN,
+    DARK_GRAY,
+    GRAY,
+    GREEN,
+    LIGHT_GRAY,
+    MAGENTA,
+    RED,
+    WHITE,
+    YELLOW,
+)
+
+__all__ = [
+    "Color",
+    "WHITE",
+    "BLACK",
+    "RED",
+    "GREEN",
+    "BLUE",
+    "YELLOW",
+    "CYAN",
+    "MAGENTA",
+    "GRAY",
+    "LIGHT_GRAY",
+    "DARK_GRAY",
+]
---
+++ src/machina/colors/standard_colors.py
@@ -0,0 +1,13 @@
+from .color import Color
+
+WHITE = Color(1.0, 1.0, 1.0)
+BLACK = Color(0.0, 0.0, 0.0)
+RED = Color(1.0, 0.0, 0.0)
+GREEN = Color(0.0, 1.0, 0.0)
+BLUE = Color(0.0, 0.0, 1.0)
+YELLOW = Color(1.0, 1.0, 0.0)
+CYAN = Color(0.0, 1.0, 1.0)
+MAGENTA = Color(1.0, 0.0, 1.0)
+GRAY = Color(0.5, 0.5, 0.5)
+LIGHT_GRAY = Color(0.75, 0.75, 0.75)
+DARK_GRAY = Color(0.25, 0.25, 0.25)
